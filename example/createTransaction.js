178e7d0196cfbc347f5f53019fc6967d5a7576f3
Jeremy Yanik <jeremy@pivotflow.com>
2018-04-30 16:02:47 -0400

    updated example

--- example/createTransaction.js
+++ example/createTransaction.js
@@ -1,10 +1,22 @@
 const NMI = require('./nmi');
 
-NMI.transaction.create('sale', {
-  amount: 9.97,
-  redirectUrl: 'http://127.0.0.1/'
-}).then((res) => {
+(async() => {
+
+  // step 1
+  let res = await NMI.transaction.create('sale', {
+    amount: 9.97,
+    redirectUrl: 'http://127.0.0.1/'
+  });
   console.log(res);
-}).catch((err) => {
+
+  // step 2
+  // client-side
+
+  // step 3
+  let token = 'transaction-token';
+  let payment = await NMI.transaction.execute(token);
+  console.log(payment);
+
+})().catch((err) => {
   console.error(err);
 });
